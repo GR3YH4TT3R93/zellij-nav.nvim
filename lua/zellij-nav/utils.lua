243cf88a6f8ee2bce4a6ab0fae173c16a30fde92
Jonathan Loughlin <jonny@jloughlin.dev>
2024-09-12 10:38:33 -0400

    fix: update zellij action cmd to match default bindings/example (#3)

--- lua/zellij-nav/utils.lua
+++ lua/zellij-nav/utils.lua
@@ -10,7 +10,12 @@ function M.zellij_navigate(short_direction, direction)
   -- Check if window number changed
   if cur_winnr == vim.fn.winnr() then
     -- If window didn't switch, use zellij action to move focus
-    vim.fn.system("zellij action move-focus " .. direction)
+    if direction == "left" or direction == "right" then
+      -- If direction is left or right, need to be able to move tabs
+      vim.fn.system("zellij action move-focus-or-tab " .. direction)
+    else
+      vim.fn.system("zellij action move-focus " .. direction)
+    end
   end
 end
 
